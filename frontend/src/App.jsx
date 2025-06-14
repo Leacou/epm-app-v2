5464fb345cefe045f482e1f227e69ff3b65a2e86
leandro couretot <leacou@leandros-MacBook-Air.local>
2025-06-14 01:18:17 -0300

    agrego de index a home para seleccionar cuentas post login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import AppRoutes from "./routes/AppRoutes";
 
+if (window.location.pathname === "/index.html") {
+  // Redirige a "/" conservando el hash
+  window.location.replace("/" + window.location.hash);
+}
+
 function App() {
   return <AppRoutes />;
 }
